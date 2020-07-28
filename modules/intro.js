9d52f1cd22fffc57db2e239a6bafbabd2bc35002
David Sparrius <david@mstone.com.au>
2020-07-28 21:28:28 +1000

    intro greeting

---
+++ modules/intro.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+module.exports = function (app) {
+// provide nice greeting
+fs.readFile( "./assets/intro.txt", function (err, data) {
+    if (err) {
+      throw err; 
+    }
+    console.log(data.toString());
+  });
+  
+}
